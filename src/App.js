e034b6b0a77c36d542d9268c86745a5772fd28f3
mutantsforever <rohit.97kushwaha@gmail.com>
2018-09-27 00:29:48 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import ButtonAppBar from './Header';
+import SimpleBottomNavigation from './Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header className="App-header">
-          <h1 className="App-title">Reaction</h1>
-        </header>
-        <footer className="App-footer">
-          <h1 className="App-bottom">@ This is not Important</h1>
-        </footer>
+        <ButtonAppBar />
+        <SimpleBottomNavigation />
       </div>  
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const styles = {
+     root: {
+        backgroundColor: '#eee8e7',
+        borderTop: "1px solid #E7E7E7",
+        textAlign: "center",
+        // padding: "20px",
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        height: "60px",
+        width: "100%",
+ 
+
+    //   flexGrow: 1,
+    // },
+    // flex: {
+    //   flexGrow: 1,
+    // },
+    // menuButton: {
+    //   marginLeft: -12,
+    //   marginRight: 20,
+     },
+    
+};
+
+class SimpleBottomNavigation extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation
+        value={value}
+        onChange={this.handleChange}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    );
+  }
+}
+
+SimpleBottomNavigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleBottomNavigation);
