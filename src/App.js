932818e0b3ce1e9970dd946d2363c71b464c2b05
rohkushw <rohit.kushwaha@capgemini.com>
2018-11-19 17:27:03 +0530

    Deleted and added from material-ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import ButtonAppBar from './Header';
+import Login from './Login';
 import SimpleBottomNavigation from './Footer';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props){
     super(props);
-  }
+    this.state = {
+        showLogin: false  // set a value in state to store whether or
+                          // not to show the Modal
+    };
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event) {  // switch the value of the showModal state
+    this.setState({
+        showLogin: !this.state.showLogin
+    });
+  }
+  getComponent() {
+    if (this.state.showLogin) {  // show the modal if state showModal is true
+      return <Login/>;
+    } else {
+      return null;
+    }
+  }
     render(){
         return(
             <div>
               <ButtonAppBar />
-              
+              <Button onClick={this.handleClick} label="Action"/>
+                {this.getComponent}
               <SimpleBottomNavigation />
             </div>
         );
--- src/Login.js
+++ src/Login.js
@@ -1,97 +1,75 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-// import axios from 'axios';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
 
-class Login extends Component {
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:''
-  }
- }
-render() {
-    return (
-      <div>
-        <MuiThemeProvider>
-          <div>
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelFocusStyle={style.floatingLabelFocusStyle}
-             underlineFocusStyle={style.underlineStyle}
-             floatingLabelText="Username"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               floatingLabelFocusStyle={style.floatingLabelFocusStyle}
-               underlineFocusStyle={style.underlineStyle}
-               hintText="Enter the Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit"
-             style={style}
-             
-             buttonStyle={{ borderRadius: 25 }}
-             backgroundColor= '#342c5c'
-             labelColor= '#fff'
-             onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      </div>
-    );
+class Login extends React.Component {
+    state = {
+      username: '',
+      password: '',
+    };
 
-    
-  }
+    handleChange = name => event => {
+        this.setState({
+          [name]: event.target.value,
+        });
+      };    
 
-//   handleClick(event){
-//     var apiBaseUrl = "http://localhost:4000/api/";
-//     var self = this;
-//     var payload={
-//     "email":this.state.username,
-//     "password":this.state.password
-//     }
-//     axios.post(apiBaseUrl+'login', payload)
-//     .then(function (response) {
-//     console.log(response);
-//     if(response.data.code == 200){
-//     console.log("Login successfull");
-//     var uploadScreen=[];
-//     uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-//     self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-//     }
-//     else if(response.data.code == 204){
-//     console.log("Username password do not match");
-//     alert("username password do not match")
-//     }
-//     else{
-//     console.log("Username does not exists");
-//     alert("Username does not exist");
-//     }
-//     })
-//     .catch(function (error) {
-//     console.log(error);
-//     });
-//     }
+    render() {
+        const { classes } = this.props;
+
+        return(
+            <form className={classes.container} noValidate autoComplete="off">
+
+                <TextField
+                required
+                id="standard-username"
+                label="Username"
+                placeholder="Username"
+                className={classes.textField}
+                margin="normal"
+                />
+
+                <TextField
+                required
+                id="standard-password-input"
+                label="Password"
+                className={classes.textField}
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                />
+
+                <Button variant="contained" color="primary" className={classes.button}>
+                    Submit
+                </Button>
+            </form>
+        );
+    }
 }
-
-
-const style = {
- margin: 15,
- floatingLabelFocusStyle: {
-  color: '#272d50',
-},
-underlineStyle: {
-  borderColor: '##272d50',
-},
-borderRadius: 25,
-
-
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
-export default Login;
+
+export default withStyles(styles)(Login);
